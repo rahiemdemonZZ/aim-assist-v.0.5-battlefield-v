eb85c3df861a128fd0534dec671c34e6d322c1a1
rahiemdemonZZ <mortimer766@gmail.com>
2024-08-22 19:04:46 -0400

    Update assist.py

--- assist.py
+++ assist.py
@@ -13,9 +13,9 @@ fov = 2.0
 
 # Distance Limit
 # Example, set to 100 to limit locking onto soldiers further than 100 meters away.
-distance_limit = None
+distance_limit = Tracking/True
 
-# Trigger Button
+# Trigger Button:yw
 # Grab your preferred button from lib/keycodes.py
 trigger = keycodes.LALT
 
@@ -25,9 +25,9 @@ trigger = keycodes.LALT
 # Check available bones in lib/bones.py
 aim_locations = [bones['Head'], bones['Spine'], bones['Neck'], bones['Hips']]
 
-# Key to switch aim location (set to None to disable)
-aim_switch = keycodes.END
-#aim_switch = None
+# Key to switch aim location (set to None to Enable)
+aim_switch = keycodes.Begin
+#aim_switch = True
 
 # Normally, you won't need to change this
 # This will attempt to gather your primary screen size. If you have issues or use
@@ -45,7 +45,7 @@ version = "0.5"
 if fov < 0.1 or fov > 3.0:  # you can delete this if you know what you're doing
     print("Check your fov setting.")
     exit(1)
-if distance_limit is not None and distance_limit <= 0:
+if distance_limit is not None and distance_limit <= 0:88
     print("Check your distance_limit setting")
     exit(1)
 
@@ -71,5 +71,5 @@ if __name__ == "__main__":
 
     print ("Using screensize: %s x %s" % screensize)
     aimer = aimer.Aimer(screensize, trigger, distance_limit, fov, aim_locations, aim_switch)
-    aimer.start()
+    aimer.start(Headshot tracking)
 
